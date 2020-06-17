adab962388f1a7f4cc1974a118a2e2e951e12104
DashaVasilyeva <da3127@yandex.ru>
2020-06-17 19:16:18 +0300

    CHATBOT-database-connector - added database connector

--- dialogManagement/telegramDialogHandler/telegramDialogInTestMode.py
+++ dialogManagement/telegramDialogHandler/telegramDialogInTestMode.py
@@ -1,22 +1,32 @@
 import configparser
 import telebot
+from dialogManagement.databaseConnector.DatabaseConnector import DatabaseConnector
 
 config = configparser.ConfigParser()
-config.read('../../resources/main_config.txt')
+config.read('../../resources/configfile.txt')
 
 bot = telebot.TeleBot(config['Telegram']['TelegramBotToken'])
 
 INFORM_MESSAGE = 'Test mode. Please send a message to determine your intent.'
+database_connector = DatabaseConnector()
+ERROR_MESSAGE = 'ERROR'
+
+message_id = None
 
 @bot.message_handler(commands=['start'])
-def send_welcome(message):
+def send_welcome_and_inform(message):
     bot.send_message(message.from_user.id, INFORM_MESSAGE)
 
 
 @bot.message_handler(content_types=['text'])
 def get_text_message(message):
-    print(message)
-    bot.send_message(message.from_user.id,  'Bot received your message')
-
+    result = database_connector.save_message(message.json)
+    if result != ERROR_MESSAGE:
+        bot.send_message(message.from_user.id,  'Bot received your message')
+        global message_id
+        message_id = result
+    else:
+        bot.send_message(message.from_user.id, ERROR_MESSAGE)
+    
 
 bot.polling(none_stop=True, interval=0)
